ff41baee3817ab5d8f8585809355171149cd5141
ChiaKai Chang <chiakaichang@fmt.com.tw>
2025-02-27 15:17:59 +0800

    resovle m1 and add  status

--- src/main/java/com/careline/interview/test/mission1/Mission1Controller.java
+++ src/main/java/com/careline/interview/test/mission1/Mission1Controller.java
@@ -1,5 +1,27 @@
 package com.careline.interview.test.mission1;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RequestMapping("mission1")
+@RestController()
 public class Mission1Controller {
+
+    @GetMapping("hello")
+    public String hello(){
+        // 取得當前時間
+        LocalDateTime now = LocalDateTime.now();
+
+        // 設定格式 (yyyy-MM-dd HH:mm:ss)
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        // 轉換為格式化字串並輸出
+        System.out.println();
+        return "                        Hello! (" + now.format(formatter) + ")" ;
+    }
 
 }
