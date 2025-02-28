2ef1871260c6866c619451afcdb628ef96ebb475
changchiakai <changeric1031@gmail.com>
2025-02-28 23:54:07 +0800

    m10 100%

--- src/main/java/com/careline/interview/test/mission10/Mission10Controller.java
+++ src/main/java/com/careline/interview/test/mission10/Mission10Controller.java
@@ -1,8 +1,6 @@
 package com.careline.interview.test.mission10;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.concurrent.ConcurrentHashMap;
@@ -10,18 +8,10 @@ import java.util.concurrent.ConcurrentHashMap;
 @RestController
 @RequestMapping("/mission10")
 public class Mission10Controller {
-
-    private int callTime = 0;
-
-
-    // 題目有點問題
-    private static int totalCallCount = 0; // 總呼叫次數
     private static ConcurrentHashMap<String, Integer> sessionCallCounts = new ConcurrentHashMap<>(); // 存儲每個 sessionId 的呼叫次數
 
     @PostMapping("/call")
-    public String call(HttpSession session) {
-        String sessionId = session.getId();
-        System.out.println("sessionId:"+ sessionId);
+    public int call(@RequestHeader("X-Session-Id") String sessionId) {
         // 根據 sessionId 獲取當前的呼叫次數
         sessionCallCounts.putIfAbsent(sessionId, 0); // 如果該 sessionId 尚未存在，則初始化為 0
         int currentCount = sessionCallCounts.get(sessionId);
@@ -30,11 +20,8 @@ public class Mission10Controller {
         currentCount++;
         sessionCallCounts.put(sessionId, currentCount);
 
-        // 更新總呼叫次數
-        totalCallCount++;
 
-        return String.format("{\"totalCallCount\": %d, \"sessionId\": \"%s\", \"sessionCount\": %d}",
-                totalCallCount, sessionId, currentCount);
+        return currentCount;
 
     }
 
